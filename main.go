6d4b3c1c20e2906ccd3d549c01b3b8fb28d13666
Roman Kulkov <romawka2410@gmail.com>
2024-01-10 16:43:31 +0200

    log.Fatal змінено на panic

--- main.go
+++ main.go
@@ -3,40 +3,55 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"path/filepath"
 )
 
-func scanDirectory(path string) error { // створюємо рекурсію (path-шлях) і повертаємо помилку
+func reportPanic() {
+	p := recover() //створюємо функцію і викликаємо рековер
+	if p == nil {  // якщо все ок(nil) - повертаємо функцію
+		return
+	}
+	err, ok := p.(error) // створємо зміні для помилки
+	if ok {
+		fmt.Println(err) //друкуємо помилку
+	} else {
+		panic(p) //якщо якась паніка але не error, то виводимо саму паніку
+	}
+}
+
+func scanDirectory(path string) { // створюємо рекурсію (path-шлях) і повертаємо помилку
 	fmt.Println(path)
 
 	files, err := ioutil.ReadDir(path) //створюємо змінні в які будемо отримувати файли
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	for _, file := range files { // створюємо цикл з нашими файлами
 		filePath := filepath.Join(path, file.Name())
 
 		if file.IsDir() { //якщо знаходить першу директорію(папку)
-			err := scanDirectory(filePath) // знову запускаємо скан але вже в цій папці що знайшли
-			if err != nil {
-				log.Fatal(err)
-			}
+			scanDirectory(filePath) // знову запускаємо скан але вже в цій папці що знайшли
+
 		} else {
 			fmt.Println(filePath)
 		}
 
 	}
-	return nil
 }
 
 func main() {
+	defer reportPanic() //defer відкладений виклик, тобто він викличиться тоді коли закінчиться робота scanDirectory
+
+	scanDirectory("my_directory")
+}
+
+/*func main() {
 	err := scanDirectory("my_directory") // зтворюємо зміну "помилку", якщо буде помилка і викликати функцію скан
 	// з каталогом в якому проводимо сканування
 	if err != nil { //  і обробляємо помилку
 		log.Fatal(err)
 	}
-}
+}*/
 
 /*func main() {
 	files, err := ioutil.ReadDir("my_directory") //створюємо змінні в які будемо отримувати файли
